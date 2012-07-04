1bdf905e9876fb35cdfe74bf01097afcf043ed2c
Vlad Seryakov <vseryakov@gmail.com>
2012-07-04 01:23:10 -0400

    delete user with all resources

--- ElasticWolf/chrome/content/views/iam.js
+++ ElasticWolf/chrome/content/views/iam.js
@@ -75,6 +75,27 @@ var ew_UsersTreeView = {
         var item = this.getSelected();
         if (!item) return;
         if (!confirm("Delete user?")) return;
+        if (item.loginProfileDate) {
+            this.core.api.deleteLoginProfile(item.name);
+        }
+        if (item.policies) {
+            for (var i in item.policies) {
+                this.core.api.deleteUserPolicy(item.name, item.policies[i]);
+            }
+            sleep(1000)
+        }
+        if (item.accessKeys) {
+            for (var i in item.accessKeys) {
+                this.core.api.deleteAccessKey(item.accessKeys[i].id, item.name);
+            }
+            sleep(1000)
+        }
+        if (item.groups) {
+            for (var i in item.groups) {
+                this.core.api.removeUserFromGroup(item.groups[i].name, item.name);
+            }
+            sleep(1000)
+        }
         this.core.api.deleteUser(item.name, function() {
             if (me.core.removeModel('users', item.name, 'name')) me.invalidate(); else me.refresh();
         });
@@ -197,7 +218,7 @@ var ew_UsersTreeView = {
         } else {
             if (!confirm('Delete policy ' + item.policies[idx] + '?')) return;
         }
-        this.core.api.deleteUserPolicy(item.name, item.policies[idx], text, function() {
+        this.core.api.deleteUserPolicy(item.name, item.policies[idx], function() {
             item.policies = null;
             me.selectionChanged();
         });
