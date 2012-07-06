9abe564a99a27efe56fbc72a3ca39278e8506e33
Vlad Seryakov <vseryakov@gmail.com>
2012-07-06 16:16:49 -0400

    fixed S3 navigation

--- ElasticWolf/chrome/content/views/s3.js
+++ ElasticWolf/chrome/content/views/s3.js
@@ -49,20 +49,25 @@ var ew_S3BucketsTreeView = {
         var idx = -1;
         var path = this.path.join("/") + "/";
         var nlist = [];
+        var labels = {};
         for (var i in list) {
-            if (list[i].name.match('log')) continue;
-            var n = (this.path[0] + "/" + list[i].name).replace(/[ \/]+$/, '');
-            var p = n.split("/");
-            //debug(path + ":" + n + ":" + p.length + ":" + this.path.length);
-            // Next level only
-            if (!this.path.length || n.indexOf(path) == 0 && p.length == this.path.length + 1) {
-                list[i].folder = p[p.length - 1];
-                list[i].label = list[i].folder + (list[i].name[list[i].name.length - 1] == "/" ? "/" : "")
-                nlist.push(list[i])
-                // Select given item
-                if (list[i].name == this.folder) {
-                    idx = nlist.length - 1;
-                }
+            if (!this.path.length) {
+                list[i].folder = list[i].label = list[i].name;
+                nlist.push(list[i]);
+            } else {
+                var n = this.path[0] + "/" + list[i].name;
+                var p = n.split("/");
+                if (n <= path || n.indexOf(path) != 0) continue;
+                list[i].folder = list[i].label = p.slice(this.path.length, this.path.length + 1).join("/");
+                if (p.length > this.path.length + 1) list[i].label += "/";
+                // Skip subfolders
+                if (labels[list[i].label]) continue;
+                labels[list[i].label] = 1;
+                nlist.push(list[i]);
+            }
+            // Select given item
+            if (list[i].label == this.folder) {
+                idx = nlist.length - 1;
             }
         }
         TreeView.display.call(this, nlist);
