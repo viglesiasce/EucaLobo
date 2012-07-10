2640a0adfe9d4bb4a0da75099237d1fd52c6bd79
Vlad Seryakov <vseryakov@gmail.com>
2012-07-09 22:26:20 -0700

    fixed name regexp fixed creating new image name error fixed switching to instance tab on new instance

--- ElasticWolf/chrome/content/views/ec2.js
+++ ElasticWolf/chrome/content/views/ec2.js
@@ -174,7 +174,7 @@ var ew_AMIsTreeView = {
                 } else {
                     ew_InstancesTreeView.refresh();
                 }
-                me.core.selectTab('ew.tabs.instance' + (this.core.isVpcMode() ? ".vpc" : ""));
+                me.core.selectTab('ew.tabs.instance' + (me.core.isVpcMode() ? ".vpc" : ""));
             });
         }
     },
@@ -326,7 +326,7 @@ var ew_InstancesTreeView = {
         if (instance == null) return;
 
         var values = this.core.promptInput('Create AMI', [{label:"Instance",type:"label",value:instance.toString()},
-                                                          {label:"AMI Name",type:"name",required:1},
+                                                          {label:"AMI Name",required:1},
                                                           {label:"Description"},
                                                           {label:"Snapshot without rebooting instance",type:"checkbox"} ]);
         if (!values) return;
@@ -401,7 +401,10 @@ var ew_InstancesTreeView = {
                 return false;
             }
         }
-        this.core.api.associateAddress(eip, instance.id, null, function() { me.refresh() });
+        this.core.api.associateAddress(eip, instance.id, null, function() {
+            me.refresh();
+            me.core.refreshModel('addresses');
+        });
     },
 
     retrieveRSAKeyFromKeyFile : function(keyFile, fSilent)
