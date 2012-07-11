4d91b6312f8f2f399023b194272db241ecf42c6c
Vlad Seryakov <vseryakov@gmail.com>
2012-07-11 10:12:43 -0700

    fixed adding policy

--- ElasticWolf/chrome/content/views/iam.js
+++ ElasticWolf/chrome/content/views/iam.js
@@ -173,7 +173,7 @@ var ew_UsersTreeView = {
         var item = this.getSelected();
         if (!item) return;
         var values = this.core.promptInput('Add Policy', [{label:"Policy name",type:"name",required:1},
-                                                          {label:"Policy Permissions",multiline:true,cols:50,rows:20,required:1,value:policy},
+                                                          {label:"Policy Permissions",multiline:true,cols:50,rows:20,required:1},
                                                           {label:"Policy Types",type:"menulist",list:this.core.getPolicyTypes(),required:1,onselect:"rc.items[1].obj.value=formatJSON(rc.items[2].obj.value)"}]);
         if (!values) return;
         this.core.api.putUserPolicy(item.name, values[0], values[1], function() {
@@ -473,7 +473,7 @@ var ew_GroupsTreeView = {
         var item = this.getSelected();
         if (!item) return;
         var values = this.core.promptInput('Add Policy', [{label:"Policy name",type:"name",required:1},
-                                                          {label:"Policy Permissions",multiline:true,cols:50,rows:20,required:1,value:policy},
+                                                          {label:"Policy Permissions",multiline:true,cols:50,rows:20,required:1},
                                                           {label:"Policy Types",type:"menulist",list:this.core.getPolicyTypes(),required:1,onselect:"rc.items[1].obj.value=formatJSON(rc.items[2].obj.value)"}]);
         if (!values) return;
         this.core.api.putGroupPolicy(item.name, values[0], values[1], function() {
@@ -622,7 +622,7 @@ var ew_RolesTreeView = {
        var item = this.getSelected();
        if (!item) return;
        var values = this.core.promptInput('Add Policy', [{label:"Policy name",type:"name",required:1},
-                                                         {label:"Policy Permissions",multiline:true,cols:50,rows:20,required:1,value:policy},
+                                                         {label:"Policy Permissions",multiline:true,cols:50,rows:20,required:1},
                                                          {label:"Policy Types",type:"menulist",list:this.core.getPolicyTypes(),required:1,onselect:"rc.items[1].obj.value=formatJSON(rc.items[2].obj.value)"}]);
        if (!values) return;
        this.core.api.putRolePolicy(item.name, values[0], values[1], function() {
