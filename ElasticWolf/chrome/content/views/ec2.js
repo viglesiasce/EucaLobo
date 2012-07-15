134dfa2361f0f4d81a832f6be266aa9d708a84cf
Vlad Seryakov <vseryakov@gmail.com>
2012-07-15 11:01:51 -0700

    Fixed syntax error in delete AMI

--- ElasticWolf/chrome/content/views/ec2.js
+++ ElasticWolf/chrome/content/views/ec2.js
@@ -218,7 +218,7 @@ var ew_AMIsTreeView = {
     deregisterImage : function(confirmed)
     {
         var me = this;
-        var image = this.getSelecteed();
+        var image = this.getSelected();
         if (!image) return;
         if (!confirmed && !confirm("Deregister AMI " + image.id + " (" + image.location + ")?")) return;
         this.core.api.deregisterImage(image.id, function() {me.refresh()});
