14670f857c084f9ff71a1880e1d96228522058bb
Vlad Seryakov <vseryakov@gmail.com>
2013-01-31 23:48:36 -0500

    rework EMR start dialog to support 2 bootsrap actgions and 2 initial steps, the idea here is to run a job with keep alive flag and add more steps later using menu, this will kepp the UI simple. The actual startup wizard like on the AWS console is pretty hge work.

--- ElasticWolf/chrome/content/views/emr.js
+++ ElasticWolf/chrome/content/views/emr.js
@@ -27,16 +27,25 @@ var ew_JobFlowsTreeView = {
                       {label:"Termination Protection", type:"checkbox",prefix:"Instances.",tooltiptext:"A Boolean that indicates whether to protect the job flow and prevent the Amazon EC2 instances in the cluster from shutting down due to API calls, user intervention, or job-flow error."},
                       {label:"Availability Zone",type:"menulist",prefix:"Instances.Placement.",list:this.core.queryModel('availabilityZones'),tooltiptext:"Specifies the Availability Zone the job flow will run in."},
                       {label:"Keep Job Flow Alive When No Steps",type:"checkbox",prefix:"Instances.",tooltiptext:"Specifies whether the job flow should terminate after completing all steps."},
-                      {label:"Ec2 Key Name",type:"menulist",prefix:"Instances.",list:this.core.queryModel("keypairs"),tooltiptext:"Specifies the name of the Amazon EC2 key pair that can be used to ssh to the master node as the user called hadoop."},
+                      {label:"Ec2 Key Name",type:"menulist",prefix:"Instances.",list:this.core.queryModel("keypairs"),key:'name',tooltiptext:"Specifies the name of the Amazon EC2 key pair that can be used to ssh to the master node as the user called hadoop."},
                       {label:"Ec2 Subnet Id",type:"menulist",prefix:"Instances.",list:this.core.queryModel("subnets"),style:"max-width:350px;",tooltiptext:"To launch the job flow in Amazon Virtual Private Cloud (Amazon VPC), set this parameter to the identifier of the Amazon VPC subnet where you want the job flow to launch. If you do not specify this value, the job flow is launched in the normal Amazon Web Services cloud, outside of an Amazon VPC. Amazon VPC currently does not support cluster compute quadruple extra large (cc1.4xlarge) instances. Thus you cannot specify the cc1.4xlarge instance type for nodes of a job flow launched in a Amazon VPC."},
                       {label:"Hadoop Version",type:"menulist",prefix:"Instances.",list:["0.18", "0.20", "0.20.205"],required:true},
                       {label:"Ami Version",type:"menulist",list:["1.0", "2.0", "latest"],tooltiptext:"The version of the Amazon Machine Image (AMI) to use when launching Amazon EC2 instances in the job flow. The following values ane valid: latest (latest AMI version; currently AMI 2.0, Hadoop 0.20.205), 2.0 (AMI 2.0, Hadoop 0.20.205), 1.0 (AMI 1.0, Hadoop 0.18), If this value is not specified, the job flow uses the default of (AMI 1.0, Hadoop 0.18)."},
                       {label:"Log Uri",tooltiptext:"Specifies the location in Amazon S3 to write the log files of the job flow. If a value is not provided, logs are not created."},
-                      {label:"Supported Products",type:"listview",rows:3,headers:["","Product"],list:["karmasphere-enterprise-utility","mapr-m3","mapr-m5"],toltiptext:"A list of strings that indicates third-party software to use with the job flow."},
+                      {label:"Supported Products",type:"listview",rows:2,headers:["","Product"],list:["karmasphere-enterprise-utility","mapr-m3","mapr-m5"],toltiptext:"A list of strings that indicates third-party software to use with the job flow."},
+                      {label:"Instance Group",type:"section"},
+                      {label:"Name",tooltiptext:"Friendly name given to the instance group"},
+                      {label:"Instance Count",type:"number",prefix:'Instances.InstanceGroup.',min:1,tooltiptext:"Target number of instances for the instance group."},
+                      {label:"Instance Type",type:"menulist",prefix:"Instances.InstanceGroup.",list:this.core.getInstanceTypes(),style:"max-width:350px;",tooltiptext:"The Amazon EC2 instance type for instance group instances"},
+                      {label:"Instance Role",type:"menulist",prefix:'Instances.InstanceGroup.',list:['MASTER','CORE','TASK'],toltiptext:"The role of the instance group in the cluster."},
+                      {label:"Bid Price",type:'number',tooltiptext:"Bid price for each Amazon EC2 instance in the instance group when launching nodes as Spot Instances, expressed in USD."},
+                      {label:"Market",type:'menulist',list:['ON_DEMAND','SPOT'],tooltiptext:"Market type of the Amazon EC2 instances used to create a cluster node."},
                       {label:"Bootstrap",type:"section"},
-                      {label:"Name",prefix:"BootstrapActions.member.1.",tooltiptext:"Name for the bootstrap action"},
-                      {label:"Path",prefix:"BootstrapActions.member.1.",tooltiptext:"Location of the script to run during a bootstrap action. Can be either a location in Amazon S3 or on a local file system."},
-                      {label:"Args",prefix:"BootstrapActions.member.1.",multiline:true,rows:3,cols:60,tooltiptext:"A list of command line arguments to pass to the bootstrap action script."},
+                      {label:"Action1",prefix:"BootstrapActions.member.1.",tooltiptext:"Location of the script to run during a bootstrap action. Can be either a location in Amazon S3 or on a local file system. Specify full path for the command and arguments separated by spaces"},
+                      {label:"Action2",prefix:"BootstrapActions.member.2.",tooltiptext:"Location of the script to run during a bootstrap action. Can be either a location in Amazon S3 or on a local file system. Specify full path to the command and arguments separated by spaces"},
+                      {label:"Steps",type:"section"},
+                      {label:"Step1",prefix:"Steps.member.1.",tooltiptext:"Separated by spaces: A path to a JAR file. Then name of the main class in the specified Java file. Then a list of command line arguments passed to the JAR file's main function when executed"},
+                      {label:"Step2",prefix:"Steps.member.2.",tooltiptext:"Separated by spaces: A path to a JAR file. Then name of the main class in the specified Java file. Then a list of command line arguments passed to the JAR file's main function when executed"},
                       ];
         var values = this.core.promptInput("Create Job Flow", inputs);
         if (!values) return;
@@ -52,7 +61,29 @@ var ew_JobFlowsTreeView = {
                 break;
 
             default:
-                opts[label] = values[i];
+                switch (inputs[i].label) {
+                case "Action1":
+                case "Action2":
+                    var list = values[i].split(" ");
+                    if (!list.length) break;
+                    opts[inputs[i].prefix + "Name"] = list[0];
+                    opts[inputs[i].prefix + "ScriptBootstrapAction.Path"] = list[0];
+                    if (list.length > 1) opts[inputs[i].prefix + "ScriptBootstrapAction.Args"] = list.slice(1).join(",");
+                    break;
+
+                case "Step1":
+                case "Step2":
+                    var list = values[i].split(" ");
+                    if (list.length < 2) break;
+                    opts[inputs[i].prefix + "Name"] = list[0];
+                    opts[inputs[i].prefix + "HadoopJarStep.Jar"] = list[0];
+                    opts[inputs[i].prefix + "HadoopJarStep.MainClass"] = list[1];
+                    if (list.length > 2) opts[inputs[i].prefix + "HadoopJarStep.Args"] = list.slice(2).join(",");
+                    break;
+
+                default:
+                    opts[label] = values[i];
+                }
             }
         }
         this.core.api.runJobFlow(values[0], values[1], opts, function() { me.refresh() });
@@ -126,7 +157,7 @@ var ew_JobFlowsTreeView = {
                  {label:"Jar",tooltiptext:"A path to a JAR file run during the step."},
                  {label:"MainClass",tooltiptext:"The name of the main class in the specified Java file. If not specified, the JAR file should specify a Main-Class in its manifest file."},
                  {label:"Args",multiline:true,rows:3,cols:60,tooltiptext:"A list of command line arguments passed to the JAR file's main function when executed"},
-                 {label:"Propertis",multiline:true,rows:3,cols:60,tooltiptext:"A list of Java properties that are set when the step runs.You can use these properties to pass key value pairs to your main function.Format is: key=value"}
+                 {label:"Properties",multiline:true,rows:3,cols:60,tooltiptext:"A list of Java properties that are set when the step runs.You can use these properties to pass key value pairs to your main function.Format is: key=value"}
                  ]);
         if (!values) return;
         var step = {}
