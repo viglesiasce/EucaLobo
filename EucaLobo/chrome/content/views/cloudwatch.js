80a1b0dcad5017781b9a4b65e3ef6a336025edad
Vic Iglesias <viglesiasce@gmail.com>
2013-07-14 12:00:20 -0700

    Metric name needs to be customizable as well.

--- EucaLobo/chrome/content/views/cloudwatch.js
+++ EucaLobo/chrome/content/views/cloudwatch.js
@@ -176,7 +176,7 @@ var ew_MetricAlarmsTreeView = {
         var inputs = [{label:"AlarmName",required:1},
                       {label:"AlarmDescription",maxlength:255},
                       {label:"Namespace",required:1},
-                      {label:"MetricName",type:"menulist",required:1,key:'name',style:"max-width:300px"},
+                      {label:"MetricName",required:1},
                       {label:"Dimensions",multiline:true,cols:30,rows:3,wrap:'off',help:"One name:value pair per line"},
                       {label:"ComparisonOperator",type:"menulist",list:["GreaterThanOrEqualToThreshold","GreaterThanThreshold","LessThanThreshold","LessThanOrEqualToThreshold"],required:1,tooltiptext:"The arithmetic operation to use when comparing the specified Statistic and Threshold. The specified Statistic value is used as the first operand.Type: String.Valid Values:GreaterThanOrEqualToThreshold |GreaterThanThreshold |LessThanThreshold |LessThanOrEqualToThreshold"},
                       {label:"Threshold",type:"number",decimalplaces:2,size:10,required:1,tooltiptext:"The value against which the specified statistic is compared."},
